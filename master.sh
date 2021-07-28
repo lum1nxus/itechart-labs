11dce3bc4b91c0a395ef4e31d28a29b61bd4814a
lum1nxus <72228762+lum1nxus@users.noreply.github.com>
2021-07-28 16:27:30 +0300

    feat: add master.sh file to run jenkins on server

---
+++ master.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "Updating package lists"
+sudo yum update -y
+echo "Installing wget package"
+sudo yum -y install wget
+echo "Installing repository link to the specified folder"
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+echo "Importing Jenkins public key"
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+echo "Upgrading package list"
+sudo yum upgrade
+echo "Installing Jenkins / OpenJDK"
+sudo yum -y install jenkins java-11-openjdk-devel
+echo "Reloading module files"
+sudo systemctl daemon-reload
+echo "Starting Jenkins"
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo cat >> /etc/hosts <<EOF
+#jenkins servers - forced since no DNS
+172.16.1.50     Master_Server
+172.16.1.51     Slave_Server
+EOF
+
+
+
