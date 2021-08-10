fa0d0fe5fbffad84f27e7020826545e5771cc523
lum1nxus <72228762+lum1nxus@users.noreply.github.com>
2021-08-10 14:04:28 +0300

    fix: fix master.sh script and artifactory url
    refactor!
    
    refactor!

--- master.sh
+++ master.sh
@@ -34,8 +34,13 @@ JFROG_HOME=/opt/jfrog
 sudo mkdir -p $JFROG_HOME/artifactory/var/etc/
 cd $JFROG_HOME/artifactory/var/etc/
 sudo touch ./system.yaml
-sudo chown -R 1000:100 $JFROG_HOME/artifactory/var
+sudo chown -R 1030:1030 $JFROG_HOME/artifactory/var
 sudo chmod -R 777 $JFROG_HOME/artifactory/var
-
+sudo bash -c 'cat << EOF > /opt/jfrog/artifactory/var/etc/system.yaml
+shared:
+    node:
+        ip: 172.16.1.50
+EOF'
+sudo docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-pro:latest
 
 
