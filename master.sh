c18063fb24c061546e26a690c784009214f179e7
lum1nxus <72228762+lum1nxus@users.noreply.github.com>
2021-08-12 14:15:07 +0300

    feat: add groovy script to register user in jenkins

--- master.sh
+++ master.sh
@@ -14,6 +14,12 @@ echo "Installing Jenkins / OpenJDK"
 sudo yum -y install jenkins java-11-openjdk-devel
 echo "Reloading module files"
 sudo systemctl daemon-reload
+echo "Disabling the setup wizard from the Jenkins initialization"
+sudo sed -i 's/JENKINS_ARGS=""/JENKINS_ARGS="-Djenkins.install.runSetupWizard=false"/' /etc/sysconfig/jenkins
+echo "Copying groove scripts to init.groovy.d folder"
+JENKINS_HOME=/var/lib/jenkins
+sudo mkdir -p $JENKINS_HOME/init.groovy.d
+sudo cp /vagrant/scripts/createUser.groovy $JENKINS_HOME/init.groovy.d
 echo "Starting Jenkins"
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
@@ -24,25 +30,5 @@ sudo cat >> /etc/hosts <<EOF
 172.16.1.50     MasterServer
 172.16.1.51     SlaveServer
 EOF
-sudo yum -y install yum-utils
-sudo yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce docker-ce-cli containerd.io
-sudo systemctl start docker
-sudo systemctl status docker
-sudo mkdir /opt/jfrog
-JFROG_HOME=/opt/jfrog
-sudo mkdir -p $JFROG_HOME/artifactory/var/etc/
-cd $JFROG_HOME/artifactory/var/etc/
-sudo touch ./system.yaml
-sudo chown -R 1030:1030 $JFROG_HOME/artifactory/var
-sudo chmod -R 777 $JFROG_HOME/artifactory/var
-sudo bash -c 'cat << EOF > /opt/jfrog/artifactory/var/etc/system.yaml
-shared:
-    node:
-        ip: 172.16.1.50
-EOF'
-sudo docker run --name artifactory -v $JFROG_HOME/artifactory/var/:/opt/jfrog/artifactory -d -p 8081:8081 -p 8082:8082 releases-docker.jfrog.io/jfrog/artifactory-pro:latest
 
 
