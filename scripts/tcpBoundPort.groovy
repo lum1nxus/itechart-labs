cfa34738d07805c9f08195acb81444493ad93017
lum1nxus <72228762+lum1nxus@users.noreply.github.com>
2021-08-18 14:55:36 +0300

    feat: add tcp bound port

---
+++ scripts/tcpBoundPort.groovy
@@ -0,0 +1,16 @@
+#!groovy
+import jenkins.model.*
+import java.util.logging.Logger
+
+def logger = Logger.getLogger("")
+def instance = Jenkins.getInstance()
+def current_slaveport = instance.getSlaveAgentPort()
+def defined_slaveport = 50000
+
+if (current_slaveport!=defined_slaveport) {
+instance.setSlaveAgentPort(defined_slaveport)
+logger.info("Slaveport set to " + defined_slaveport)
+}
+
+
+instance.save()
